1cae582722a3bbd58549df159c3528fc6db359f6
Gourav Desettty <gourav.desetty@gmail.com>
2024-09-02 15:40:50 +0530

    Add initial files

---
+++ script.js
@@ -0,0 +1,16 @@
+var typed = new Typed('#element', {
+    strings: ["I'm a Software Engineer", ' A Web Developer', "An AI ML Engineer"],
+    typeSpeed: 90,
+});
+
+
+const togglebtn = document.querySelector('.bars')
+const togglebtnIcon = document.querySelector('.bars i')
+const dropdownmenu = document.querySelector('.dropdown')
+
+togglebtn.onclick = function () {
+    dropdownmenu.classList.toggle('open')
+    const isOpen = dropdownmenu.classList.contains('open')
+
+    togglebtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
+}
